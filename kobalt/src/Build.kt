e7e087a15ee8b6d05db5e62546c0c18e32c4d695
groostav <geoff_groos@msn.com>
2017-10-19 17:42:32 -0700

    WIP

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,15 +1,23 @@
-import com.beust.kobalt.plugin.packaging.assemble
+import com.beust.kobalt.*
+import com.beust.kobalt.plugin.packaging.*
 import com.beust.kobalt.plugin.publish.bintray
-import com.beust.kobalt.project
+
+val dev = false
+val kobaltDep = if(dev) "kobalt" else "kobalt-plugin-api"
+
+val bs = buildScript {
+    repos("http://dl.bintray.com/cbeust/maven/")
+
+}
 
 val project = project {
     name = "kobalt-antlr"
     group = "com.beust"
     artifactId = name
-    version = "0.3"
+    version = "0.4"
 
     dependencies {
-        compile("com.beust:kobalt-plugin-api:1.0.68",
+        compile("com.beust:$kobaltDep:1.0.68",
                 "org.antlr:antlr4:4.7")
     }
 
---
+++ src/main/kotlin/com/beust/kobalt/plugin/antlr/main.kt
@@ -0,0 +1,5 @@
+package com.beust.kobalt.plugin.antlr
+
+fun main(argv: Array<String>) {
+    com.beust.kobalt.main(argv)
+}
